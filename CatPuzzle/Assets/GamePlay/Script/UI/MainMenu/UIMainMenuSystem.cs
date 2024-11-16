5551ced29001cded20150711005624f3be95db0d
behappy <1208712478@qq.com>
2024-11-16 13:35:57 +0800

    场景卸载

--- CatPuzzle/Assets/GamePlay/Script/UI/MainMenu/UIMainMenuSystem.cs
+++ CatPuzzle/Assets/GamePlay/Script/UI/MainMenu/UIMainMenuSystem.cs
@@ -1,3 +1,6 @@
+using F8Framework.Launcher;
+using UnityEngine;
+
 namespace GamePlay.Script.UI.MainMenu
 {
     public partial class UIMainMenu
@@ -5,6 +8,12 @@ namespace GamePlay.Script.UI.MainMenu
         private void OnBeginGame()
         {
             //跳转到游戏场景
+            this.UnLoad();
+            //游戏主场景 关卡选择
+            FF8.Asset.LoadAsync<GameObject>("MainMenu", (mainMenu) =>
+            {
+                GameObject obj = Instantiate(mainMenu);
+            });
         }
         
         private void OnAc()
@@ -15,6 +24,11 @@ namespace GamePlay.Script.UI.MainMenu
         private void OnExit()
         {
             //退出游戏
+        }
+
+        private void UnLoad()
+        {
+            FF8.Asset.UnloadAsync("MainMenu",false,null);
         }
     }
 }
